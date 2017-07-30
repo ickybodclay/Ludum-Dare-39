b733e2329621416f6da3c8a566de502b673ca8a1
Jason Petterson <jp@brokenshotgun.com>
2017-07-29 23:17:59 -0400

    added dumb ai, locked to 16:9 aspect ratio

--- Assets/LD39/Scripts/Enemy.cs
+++ Assets/LD39/Scripts/Enemy.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMotor : MonoBehaviour {
+public class Enemy : MonoBehaviour {
     private Rigidbody2D rb2d;
 
-    [SerializeField] private float m_MinSpeedX = 5f;
+    //[SerializeField] private float m_MinSpeedX = 5f;
     [SerializeField] private float m_MaxSpeedX = 10f;
     [SerializeField] private float m_MaxSpeedY = 6f;
     [SerializeField] private int m_MaxHealth = 20;
@@ -16,7 +16,7 @@ public class PlayerMotor : MonoBehaviour {
     private bool m_IsAttacking = false;
     [SerializeField] private float m_DashSpeed = 50f;
 
-    void Start () {
+    void Start() {
         rb2d = GetComponent<Rigidbody2D>();
 
         Reset();
@@ -26,10 +26,18 @@ public class PlayerMotor : MonoBehaviour {
         health = m_MaxHealth;
     }
 
+    private void Update() {
+        if (GameManager.Instance.player != null) {
+            Move(-Vector2.MoveTowards(transform.position, GameManager.Instance.player.transform.position, m_MaxSpeedX * Time.deltaTime));
+        }
+    }
+
+    public void Move(Vector2 move) {
+        Move(Mathf.Clamp(move.x, -1f, 1f), Mathf.Clamp(move.y, -1f, 1f));
+    }
+
     public void Move(float moveX, float moveY) {
-        rb2d.AddForce(new Vector2(
-            (moveX * m_MaxSpeedX) +
-            ((m_IsFacingRight ? 1f : -1f) * m_MinSpeedX), moveY * m_MaxSpeedY) * Time.deltaTime, ForceMode2D.Impulse);
+        rb2d.AddForce(new Vector2(moveX * m_MaxSpeedX, moveY * m_MaxSpeedY) * Time.deltaTime, ForceMode2D.Impulse);
 
         if (moveX > 0 && !m_IsFacingRight) {
             Flip();
@@ -58,11 +66,5 @@ public class PlayerMotor : MonoBehaviour {
 
     private void ResetCooldown() {
         m_IsAttacking = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.transform.tag == "Enemy") {
-            Debug.Log("Hit!  Impact speed = " + other.relativeVelocity.magnitude);
-        }
     }
 }
